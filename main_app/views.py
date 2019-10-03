f37a16c7a28172ae6b46fa1b45aeca3d68b86e6d
Samuel191 <samuel.moredi@gmail.com>
2019-10-02 19:48:02 -0500

    realize comentario

--- main_app/views.py
+++ main_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .models import Tecnico
 from .forms import TecnicoForm
-
+#realize comentario
 
 # Create your views here.
 def index(request):
